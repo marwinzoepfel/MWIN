797aa92c5052e331df6f6e0486d020afc90e503e
marwinzoepfel <marwin@zoepfel.de>
2024-07-11 08:35:30 +0200

    First really working system of messaging

--- client/network/message.go
+++ client/network/message.go
@@ -5,9 +5,12 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"os/exec"
+	"runtime"
 	"time"
 
-	"github.com/martinlindhe/notify"
+	"github.com/getlantern/systray"
+	"github.com/godbus/dbus"
 )
 
 const (
@@ -35,9 +38,63 @@ func ReceiveMessages(conn net.Conn) {
 			fmt.Println("Error printing message:", err)
 			return // Exit the goroutine if there's an error
 		}
-		fmt.Println("message: " + message)
-		notify.Notify("Benachrichtigung", message, "", "")
+		fmt.Println("Nachricht für OS: " + message)
+		sendMessageToOS(message)
+		// notify.Notify("Benachrichtigung", message, "", "")
 	}
+}
+
+func sendMessageToOS(message string) {
+	switch runtime.GOOS {
+	case "darwin":
+		// macOS: Native Benachrichtigung
+		script := fmt.Sprintf(`display notification "%s" with title "MWIN Chat"`, message)
+		cmd := exec.Command("osascript", "-e", script)
+		err := cmd.Run()
+		if err != nil {
+			fmt.Println("Error sending notification:", err)
+		}
+
+	case "linux":
+		// Linux: D-Bus oder systray
+		conn, err := dbus.SessionBus() // Hier wird conn für D-Bus benötigt
+		if err != nil {
+			systray.Run(onSystrayReady, onSystrayExit)
+			return
+		}
+		obj := conn.Object("org.freedesktop.Notifications", "/org/freedesktop/Notifications")
+		call := obj.Call("org.freedesktop.Notifications.Notify", 0, "", uint32(0),
+			"", "Neue Nachricht", message, []string{}, map[string]dbus.Variant{}, int32(-1))
+		if call.Err != nil {
+			fmt.Println("Error sending notification:", call.Err)
+		}
+
+	case "windows":
+		// Windows: Native Benachrichtigung oder systray
+		// ... (Implementierung mit golang.org/x/sys/windows oder systray)
+
+	default:
+		// Andere Plattformen: systray
+		systray.Run(onSystrayReady, onSystrayExit)
+	}
+}
+
+func onSystrayReady() {
+	systray.SetTooltip("MWIN")
+	mNewMessage := systray.AddMenuItem("Neue Nachricht", "")
+	go func() {
+		for {
+			select {
+			case msg := <-mNewMessage.ClickedCh:
+				// Handle click on "New Message" menu item
+				fmt.Println("New Message clicked:", msg)
+			}
+		}
+	}()
+}
+
+func onSystrayExit() {
+	// Keine spezifischen Aufräumarbeiten erforderlich
 }
 
 // SendMessages continuously reads messages from the user's input and sends them to the server.
