0c357eb136b2fe431078c5eb1b9c4bcd2bf24f59
Marwin Zoepfel <34795642+marwinzoepfel@users.noreply.github.com>
2024-08-10 08:05:15 +0200

    Translate message.go

--- client/network/message.go
+++ client/network/message.go
@@ -1,48 +1,48 @@
 package network
 
 import (
-	"bufio"
-	"client/notification"
-	"strings"
+    "bufio"
+    "client/notification"
+    "strings"
 
-	"fmt"
-	"net"
-	"time"
+    "fmt"
+    "net"
+    "time"
 )
 
-// ReceiveMessages empfängt Nachrichten vom Server und ruft die Callback-Funktion auf.
+// ReceiveMessages receives messages from the server and calls the callback function.
 func ReceiveMessages(conn net.Conn, onMessageReceived func(string)) {
-	reader := bufio.NewReader(conn)
-	for {
-		message, err := reader.ReadString('\n')
-		if err != nil {
-			fmt.Println("Connection to server interrupted:", err)
-			return
-		}
-		if len(message) == 0 {
-			continue
-		}
-		//currentTime := time.Now().Format("15:04:05")
-		message = message[:len(message)-1]
+    reader := bufio.NewReader(conn)
+    for {
+        message, err := reader.ReadString('\n')
+        if err != nil {
+            fmt.Println("Connection to server interrupted:", err)
+            return
+        }
+        if len(message) == 0 {
+            continue
+        }
+        //currentTime := time.Now().Format("15:04:05") 
+        message = message[:len(message)-1] 
 
-		formattedMessage := strings.ReplaceAll(message, "\033[38;5;153m%s\033", "")
-		message = fmt.Sprintf("\033[38;5;153m%s\033[0m", message)
+        formattedMessage := strings.ReplaceAll(message, "\033[38;5;153m%s\033", "") 
+        message = fmt.Sprintf("\033[38;5;153m%s\033[0m", message)
 
-		notification.SendMessageToOS(formattedMessage)
-		onMessageReceived(message)
-	}
+        notification.SendMessageToOS(formattedMessage) 
+        onMessageReceived(message)
+    }
 }
 
-// SendMessage sendet eine Nachricht an den Server und aktualisiert die GUI.
+// SendMessage sends a message to the server and updates the GUI.
 func SendMessage(conn net.Conn, message string, onMessageSent func(string)) {
-	currentTime := time.Now().Format("15:04:05")
-	formattedMessage := fmt.Sprintf("[%s] You: %s", currentTime, message)
+    currentTime := time.Now().Format("15:04:05")
+    formattedMessage := fmt.Sprintf("[%s] You: %s", currentTime, message)
 
-	onMessageSent(formattedMessage)
+    onMessageSent(formattedMessage)
 
-	_, err := fmt.Fprintln(conn, message)
-	if err != nil {
-		fmt.Println("Error sending message:", err)
-		// Hier könntest du eine Fehlerbehandlung hinzufügen, z.B. die Verbindung schließen
-	}
+    _, err := fmt.Fprintln(conn, message)
+    if err != nil {
+        fmt.Println("Error sending message:", err)
+        // You could add error handling here, e.g., closing the connection
+    }
 }
