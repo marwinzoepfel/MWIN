6202fe10c21f59ec249a7bb63e9e85708e913baf
Marwin Zoepfel <34795642+marwinzoepfel@users.noreply.github.com>
2024-08-10 08:06:59 +0200

    Update os.go

--- client/notification/os.go
+++ client/notification/os.go
@@ -1,34 +1,35 @@
 package notification
 
 import (
-	"fmt"
-	"os/exec"
-	"regexp"
-	"runtime"
+    "fmt"
+    "os/exec"
+    "regexp"
+    "runtime"
 )
 
+// SendMessageToOS sends a notification message to the operating system
 func SendMessageToOS(message string) {
-	switch runtime.GOOS {
-	case "darwin":
-		Notification_darwin_nativ(message)
-	default:
-		// Andere Plattformen: Keine Benachrichtigung (oder optionale Implementierung)
-		fmt.Println("Benachrichtigungen werden auf dieser Plattform nicht unterstützt.")
-	}
+    switch runtime.GOOS {
+    case "darwin":
+        Notification_darwin_nativ(message)
+    default:
+        // Other platforms: No notification (or optional implementation)
+        fmt.Println("Notifications are not supported on this platform.")
+    }
 }
 
-// macOS: Native Benachrichtigung
+// macOS: Native notification
 func Notification_darwin_nativ(message string) {
-	// Regulärer Ausdruck zum Erkennen von ANSI-Farbcodes
-	re := regexp.MustCompile(`\x1B\[[0-?]*[ -/]*[@-~]`)
+    // Regular expression to detect ANSI color codes
+    re := regexp.MustCompile(`\x1B\[[0-?]*[ -/]*[@-~]`)
 
-	// Entferne Farbcodes aus der Nachricht
-	cleanMessage := re.ReplaceAllString(message, "")
+    // Remove color codes from the message
+    cleanMessage := re.ReplaceAllString(message, "")
 
-	script := fmt.Sprintf(`display notification "%s" with title "MWIN Chat"`, cleanMessage)
-	cmd := exec.Command("osascript", "-e", script)
-	err := cmd.Run()
-	if err != nil {
-		fmt.Println("Error sending notification:", err)
-	}
+    script := fmt.Sprintf(`display notification "%s" with title "MWIN Chat"`, cleanMessage)
+    cmd := exec.Command("osascript", "-e", script)
+    err := cmd.Run()
+    if err != nil {
+        fmt.Println("Error sending notification:", err)
+    }
 }
